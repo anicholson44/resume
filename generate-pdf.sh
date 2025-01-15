8acb983110cc9be9bc05a554fd1ccee0a3c6c6e5
Andrew Nicholson <nicholsonsoftware@gmail.com>
2025-01-15 13:58:21 -0700

    formatting

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,1 +1,5 @@
-pandoc -f gfm -t pdf README.md -V mainfont="DejaVu Sans" -o Andrew\ Nicholson\'s\ Resume.pdf
+pandoc -f gfm \
+       -t pdf README.md \
+       -V mainfont="DejaVu Sans" \
+       -V geometry:margin=0.75in \
+       -o Andrew\ Nicholson\'s\ Resume.pdf
